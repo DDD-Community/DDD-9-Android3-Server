d6fc3fca0b1156cfba70d50334779d2d830250e5
minaamim <minaaa5814@gmail.com>
2023-06-08 02:31:46 +0900

    [chore] set initial stage

---
+++ src/test/java/com/nexters/buyornot/BuyornotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexters.buyornot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyornotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
