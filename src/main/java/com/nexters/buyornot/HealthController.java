5a230652724dc3c4e1cfa8eba2af6e7a6bce7844
minaamim <minaaa5814@gmail.com>
2023-06-20 22:28:11 +0900

    [feat] add swagger

--- src/main/java/com/nexters/buyornot/HealthController.java
+++ src/main/java/com/nexters/buyornot/HealthController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MainController {
+public class HealthController {
 
-    @GetMapping("/")
+    @GetMapping("/health")
     public String healthCheck() throws Exception{
         return "health check OK";
     }
---
+++ src/main/java/com/nexters/buyornot/config/swagger/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.nexters.buyornot.config.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Buy or Not")
+                .description("바이올낫 API docs")
+                .version("1.0.0");
+    }
+}
