9105ec57a3e448569dbf5f2f8a73fd297829ac8b
minaamim <minaaa5814@gmail.com>
2023-06-20 22:28:11 +0900

    [feat] add swagger

---
+++ src/main/java/com/nexters/buyornot/config/WebSecurityConfig.java
@@ -0,0 +1,60 @@
+package com.nexters.buyornot.config;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        log.warn("accessDeniedHandler");
+        return (request, response, e) -> {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("text/plain;charset=UTF-8");
+            response.getWriter().write("ACCESS DENIED");
+            response.getWriter().flush();
+            response.getWriter().close();
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests()
+                        .requestMatchers("/swagger-resources/**").permitAll()
+                        .anyRequest().permitAll()
+                        .and()
+                                .httpBasic().and()
+                .formLogin().disable()
+                .logout().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .headers()
+                .frameOptions().sameOrigin()
+                .cacheControl().and()
+                .xssProtection().and()
+                .httpStrictTransportSecurity().disable();
+
+        return http.build();
+    }
+
+}
