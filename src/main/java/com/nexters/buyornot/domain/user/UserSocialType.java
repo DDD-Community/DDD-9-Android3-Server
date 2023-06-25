a665b45263188d695324bd3aad7edeac95ac590a
minaamim <minaaa5814@gmail.com>
2023-06-25 18:26:20 +0900

    [feat] add user entity

---
+++ src/main/java/com/nexters/buyornot/domain/user/UserSocialType.java
@@ -0,0 +1,13 @@
+package com.nexters.buyornot.domain.user;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum UserSocialType {
+    KAKAO("카카오"),
+    GOOGLE("구글"),
+    APPLE("애플");
+
+    private final String value;
+}
