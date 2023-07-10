3a69c4e2883c82b10c9c40c82ddff82df9d7a9bf
minaamim <minaaa5814@gmail.com>
2023-07-10 22:19:53 +0900

    add archive

---
+++ src/main/java/com/nexters/buyornot/module/archive/domain/Archive.java
@@ -0,0 +1,37 @@
+package com.nexters.buyornot.module.archive.domain;
+
+import com.nexters.buyornot.module.model.BaseEntity;
+import com.nexters.buyornot.module.model.Price;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Archive extends BaseEntity {
+
+    @Id
+    @Column(name = "archive_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private UUID userId;
+
+    private Long itemId;
+
+    private String brand;
+
+    private String itemName;
+
+    @Embedded
+    private Price price;
+
+    @Lob
+    private String imgUrl;
+
+    private String imgName;
+
+    private String imgType;
+}
