b3fe3e3c8c417890e9178492d3376a7b45813b99
duckbill413 <uhyeon7399@daum.net>
2023-10-17 17:45:26 +0900

    [refactor] 불필요 import 삭제 및 트렌잭션 확인

--- src/test/java/com/nexters/buyornot/module/archive/api/ArchiveControllerTest.java
+++ src/test/java/com/nexters/buyornot/module/archive/api/ArchiveControllerTest.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
