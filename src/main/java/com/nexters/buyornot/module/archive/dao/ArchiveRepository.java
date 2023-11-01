bff1b14a783fe69a0e1dcc1296abf884e15c2711
duckbill413 <uhyeon7399@daum.net>
2023-11-01 17:21:05 +0900

    [fix] ArchiveRepository - JPQL Param value 추가

--- src/main/java/com/nexters/buyornot/module/archive/dao/ArchiveRepository.java
+++ src/main/java/com/nexters/buyornot/module/archive/dao/ArchiveRepository.java
@@ -1,6 +1,7 @@
 package com.nexters.buyornot.module.archive.dao;
 
 import com.nexters.buyornot.module.archive.domain.Archive;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,10 +16,10 @@ import java.util.UUID;
 public interface ArchiveRepository extends JpaRepository<Archive, Long> {
 
     @Query("select a from Archive a where a.userId = (:userId) and a.itemUrl = (:url)")
-    Optional<Archive> findByUserAndUrl(String userId, String url);
+    Optional<Archive> findByUserAndUrl(@Param("userId") String userId,@Param("url") String url);
 
     @Query("select a from Archive a where a.userId = (:userId) and a.itemId = (:itemId)")
-    Optional<Archive> findByUserAndItem(String userId, Long itemId);
+    Optional<Archive> findByUserAndItem(@Param("userId") String userId,@Param("itemId)") Long itemId);
 
     List<Archive> findPageByUserIdOrderByUpdatedAtDesc(String userId, Pageable pageable);
 
